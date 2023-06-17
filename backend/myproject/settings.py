1bfe3e642a6db459186d24fb78002a5b17b00648
Martin <c.martin.pal@gmail.com>
2023-06-17 18:16:15 -0300

    Changes to allow images upload

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+# Media
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 
 # Application definition
 
--- backend/myproject/urls.py
+++ backend/myproject/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = routers.DefaultRouter()
 
@@ -25,3 +27,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("tasks.urls")),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

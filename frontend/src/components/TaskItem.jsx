89e825fd173bac3d9bfc65799f2d61568d2e1358
Martin <c.martin.pal@gmail.com>
2023-06-17 20:06:49 -0300

    tasks list and items

--- frontend/src/components/TaskItem.jsx
+++ frontend/src/components/TaskItem.jsx
@@ -1,10 +1,27 @@
-const TaskItem = ({ task }) => {
-//task is an object with the following properties:
-//id, title, completed, endaDate, image.
+import TableRow from '@mui/material/TableRow';
+import { styled } from '@mui/material/styles';
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+      border: 0,
+    },
+  }));
+
+const TaskItem = ({ task, StyledTableCell }) => {
 
     return (
-        <div>
-        </div>
+        <StyledTableRow>
+            <StyledTableCell component="th" scope="row">
+                <img src={task.image} alt={task.title} />
+            </StyledTableCell>
+            <StyledTableCell>{task.title}</StyledTableCell>
+            <StyledTableCell align="right">{task.endDate}</StyledTableCell>
+            <StyledTableCell align="right">{task.completed ? 'Yes' : 'No'}</StyledTableCell>
+        </StyledTableRow>
     );
 };
 
--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -1,4 +1,23 @@
 import TaskItem from './TaskItem';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+
 
 const TaskList = ({ tasks, loading, error }) => {
     if (loading) {
@@ -10,13 +29,23 @@ const TaskList = ({ tasks, loading, error }) => {
     }
     
     return (
-        <ul>
-        {tasks.map((task) => (
-            <li key={task.id}>
-                <TaskItem task={task}></TaskItem>
-            </li>
-        ))}
-        </ul>
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 700 }} aria-label="customized table">
+                <TableHead>
+                    <TableRow>
+                        <StyledTableCell>Image</StyledTableCell>
+                        <StyledTableCell>Task</StyledTableCell>
+                        <StyledTableCell align="right">End Date</StyledTableCell>
+                        <StyledTableCell align="right">Completed</StyledTableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {tasks.map((task) => (
+                        <TaskItem key={task.id} task={task} StyledTableCell={StyledTableCell}></TaskItem>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
     );
 };
 

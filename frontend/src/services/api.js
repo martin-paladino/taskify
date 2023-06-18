e4c58ad841d5346c782e3f40aa17bfe315e6b096
Martin <c.martin.pal@gmail.com>
2023-06-18 20:11:21 -0300

    functions to make requests

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -8,4 +8,45 @@ export const fetchTasks = async () => {
     } catch (error) {
         throw error;
     }
-}
+};
+
+export const createTask = async (task) => {
+    try {
+        const res = await fetch(`${BASE_URL}/api/tasks/`, {
+            method: 'POST',
+            body: task,
+        });
+        const data = await res.json();
+        console.log("data", data)
+        return data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const updateTask = async (fields, taskId) => {
+    try {
+        const res = await fetch(`${BASE_URL}/api/tasks/${taskId}/`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(fields),
+        });
+        const data = await res.json();
+        return data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const deleteTask = async (id) => {
+    try {
+        const res = await fetch(`${BASE_URL}/api/tasks/${id}/`, {
+            method: 'DELETE',
+        });
+        return res.status === 204;
+    } catch (error) {
+        throw error;
+    }
+};

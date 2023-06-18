88cc5fcab4e9f4a038df76c0d5ee8fe4e4a3328d
Martin <c.martin.pal@gmail.com>
2023-06-18 20:07:32 -0300

    taskSerializer includes all fields and media folder added to gitignore

--- backend/tasks/serializers.py
+++ backend/tasks/serializers.py
@@ -5,4 +5,4 @@ from rest_framework import serializers
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = ("id", "title", "completed")
+        fields = '__all__'

edb8dfcff860cc36af4d0211fda37ca67d90c5f5
Martin <c.martin.pal@gmail.com>
2023-06-20 13:55:34 -0300

    register, login & logout added

--- backend/tasks/urls.py
+++ backend/tasks/urls.py
@@ -1,8 +1,20 @@
 from rest_framework import routers
-from .views import TaskViewSet
+from django.urls import path
+from .views import (
+    TaskViewSet,
+    UserRegistrationView,
+    UserLoginView,
+    UserLogoutView
+)
 
 router = routers.DefaultRouter()
 
+urlpatterns = [
+    path("register/", UserRegistrationView.as_view(), name="register"),
+    path("login/", UserLoginView.as_view(), name="login"),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
+]
+
 router.register(r"tasks", TaskViewSet)
 
-urlpatterns = router.urls
+urlpatterns += router.urls
--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,9 +1,72 @@
 from rest_framework import viewsets
+from rest_framework.views import APIView
+
 from .models import Task
+from django.contrib.auth.models import User
 from .serializers import TaskSerializer
+
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import authenticate, login, logout
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
 
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all().order_by("id")
     serializer_class = TaskSerializer
 
+
+class UserRegistrationView(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        email = request.data.get("email")
+
+        # Perform validation on the input data
+        try:
+            validate_email(email)
+        except ValidationError:
+            return Response(
+                {"message": "Invalid email address"}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        if User.objects.filter(email=email).exists():
+            return Response(
+                {"message": "Email already exists"}, status=status.HTTP_400_BAD_REQUEST
+            )
+        if User.objects.filter(username=username).exists():
+            return Response(
+                {"message": "Username already exists"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        # Create a new user if the input data is valid
+        user = User.objects.create_user(
+            username=username, password=password, email=email
+        )
+
+        return Response({"message": "User registered successfully"})
+
+
+class UserLoginView(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return Response({"message": "User logged in successfully"})
+        else:
+            return Response(
+                {"message": "Invalid username or password"},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+
+
+class UserLogoutView(APIView):
+    def post(self, request):
+        logout(request)
+        return Response({"message": "User logged out successfully"})
